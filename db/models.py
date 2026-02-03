3255214d18d98266d5d7d486b0a4b87e9b8b2b33
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-02-03 15:37:20 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
--- main.py
+++ main.py
@@ -2,6 +2,31 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Genre, Actor
+
+genres = ["Western", "Action", "Dramma"]
+actors = [
+    "George Klooney",
+    "Kianu Reaves",
+    "Scarlett Keegan",
+    "Will Smith",
+    "Jaden Smith",
+    "Scarlett Johansson"
+]
 
 def main() -> QuerySet:
-    pass
+    for genre in genres:
+        Genre.objects.create(name=genre)
+
+    for actor in actors:
+        name, surname = actor.split()
+        Actor.objects.create(first_name=name, last_name=surname)
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
